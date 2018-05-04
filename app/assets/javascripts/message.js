32275b56576f4d9de010289a47ad3d4ee3316635
verdiego <mnverdiego@gmail.com>
2018-05-04 20:14:43 +0900

    メッセージに画像がある場合とない場合で、非同期で追加させるhtmlを分岐

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,22 +1,45 @@
 $(function(){
   function buildHTML(message){
-    var html = `<div class="message-zone__message">
-                  <div class="message-zone__message__member">
-                    <span>
-                      ${message.user.name}
-                    </span>
-                  </div>
-                  <div class="message-zone__message__date">
-                    <span>
-                      ${message.created_at}
-                    </span>
-                  </div>
-                  <div class="message-zone__message__text">
-                    <span>
-                      ${message.content}
-                    </span>
-                  </div>
-                </div>`
+    console.log($('.message-zone').height());
+    if (message.image.url != null) {
+      console.log(message.image);
+      var html = `<div class="message-zone__message">
+                    <div class="message-zone__message__member">
+                      <span>
+                        ${message.user.name}
+                      </span>
+                    </div>
+                    <div class="message-zone__message__date">
+                      <span>
+                        ${message.created_at}
+                      </span>
+                    </div>
+                    <div class="message-zone__message__text">
+                      <span>
+                        ${message.content}
+                      </span>
+                      <img class=".message-zone__message__text" src="${message.image.url}" alt="${message.image}">
+                    </div>
+                  </div>`
+    }else{
+      var html = `<div class="message-zone__message">
+                    <div class="message-zone__message__member">
+                      <span>
+                        ${message.user.name}
+                      </span>
+                    </div>
+                    <div class="message-zone__message__date">
+                      <span>
+                        ${message.created_at}
+                      </span>
+                    </div>
+                    <div class="message-zone__message__text">
+                      <span>
+                        ${message.content}
+                      </span>
+                    </div>
+                  </div>`
+    }
     return html;
   }
   $('#new_message').on('submit', function(e){
