7966dd5fbdef9a4ce6ac54c9092b2c3a608b209d
verdiego <mnverdiego@gmail.com>
2018-05-08 20:59:08 +0900

    HTMLを追加するメソッドを作成

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,7 +1,19 @@
 $(function(){
+  function appendUser(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
+                </div>`
+    return $("#user-search-result").append(html);
+  }
+  function appendNoUser(user){
+    var html = `<div class="chat-group-user clearfix">
+                  ${user}
+                </div>`
+    return $("#user-search-result").append(html);
+  }
   $('#user-search-field').on('keyup', function(){
     var input = $("#user-search-field").val();
-    console.log(input);
     $.ajax({
       url: '/user',
       type: "GET",
