80d502eed3626216fd997630fd5f1f5c69092fb6
verdiego <mnverdiego@gmail.com>
2018-05-14 21:31:28 +0900

    ルーティングでindexを一番前にした

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :user, only: [:edit, :update, :index]
+  resources :user, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
