d1583d794553d5066301620c4dd0f923b868e9c3
verdiego <mnverdiego@gmail.com>
2018-05-15 20:27:03 +0900

    不要なreturnを削除

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -5,13 +5,13 @@ $(document).on('turbolinks:load', function(){
                     <p class="chat-group-user__name">${user.name}</p>
                     <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
                   </div>`
-      return $("#user-search-result").append(html);
+      $("#user-search-result").append(html);
     }
     function appendNoUser(comment){
       var html = `<div class="chat-group-user clearfix">
                     ${comment}
                   </div>`
-      return $("#user-search-result").append(html);
+      $("#user-search-result").append(html);
     }
     function appendUserToMember(user_name, user_id){
       var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
@@ -19,7 +19,7 @@ $(document).on('turbolinks:load', function(){
                     <p class='chat-group-user__name'>${user_name}</p>
                     <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
                   </div>`
-      return $("#chat-group-users").append(html);
+      $("#chat-group-users").append(html);
     }
 
     $('#user-search-field').on('keyup', function(){
