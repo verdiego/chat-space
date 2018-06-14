ce5f94b4b9e60f3440f65ba3ba963f8c1692bd34
verdiego <mnverdiego@gmail.com>
2018-06-14 12:23:14 +0900

    carrierwaveの設定を修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,11 +4,12 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   config.storage = :fog
+  config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'us-east-1'
+    region: 'ap-northeast-1'
   }
 
   config.fog_directory  = 'verdiego-chat-space'
